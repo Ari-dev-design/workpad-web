0164a3acaac2da00741b56f99456cc3ac7ef1e2e
QuevedoDV <aridaneq@mail.com>
2026-02-08 23:18:01 +0000

    Fix: Diseño movil y error 404

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,8 +4,7 @@ export default function Layout({ children }) {
   return (
     <div className="min-h-screen bg-gray-50">
       <Sidebar />
-
-      <div className="ml-64 p-8">{children}</div>
+      <div className="ml-0 md:ml-64 p-4 md:p-8">{children}</div>
     </div>
   );
 }
